68921e2f380487508666286f88f0ff244aff00d8
Rafiqul Islam <rafiquljakir@gmail.com>
2023-10-24 23:09:25 +0600

    solve errorand add new function

--- MediBox.ino
+++ MediBox.ino
@@ -1,12 +1,13 @@
+
 #include <ESP8266WiFi.h>
-#include <FirebaseESP8266.h>
 #include <Servo.h>
 #include <TimeLib.h>
 #include <SoftwareSerial.h>
-#include <FirebaseArduino.h>
+#include <FirebaseESP8266.h>
 #include <ArduinoJson.h>
 #include <ESP8266HTTPClient.h>
-
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
 #define FIREBASE_HOST "medibox-45997-default-rtdb.firebaseio.com/"
 #define FIREBASE_AUTH "jvl3BVlcV7frNx9wmv2yqOodTUlTnoVhaJj1mL4t"
@@ -19,17 +20,14 @@ int buzzerPin = 16; // D0
 int switchPin = 5; // D1
 int drawers[] = {4, 0, 2, 14, 12, 13}; // Drawer pins(D2, D3,D4,D5,D6,D7)
 int drawer_Open_Duration_Time = 300000; // 5 minutes in milliseconds
-int sokale_khawar_age, sokale_khawar_pore, dupure_khawar_age, dupure_khawar_pore, rate_khawar_age, rate_khawar_pore;
-int beepDuration = 100;
-
-/*
-// Firebase credentials
-const char* firebaseHost = "medibox-45997-default-rtdb.firebaseio.com/";
-const char* firebaseAuth = "jvl3BVlcV7frNx9wmv2yqOodTUlTnoVhaJj1mL4t";
-*/
+String sokale_khawar_age, sokale_khawar_pore, dupure_khawar_age, dupure_khawar_pore, rate_khawar_age, rate_khawar_pore;
 
 
 FirebaseData firebaseData;
+FirebaseJson json;
+const long utcOffsetInSeconds = 21600; // UTC offset for your timezone (in seconds)
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
 
 void setup() {
@@ -48,38 +46,26 @@ void setup() {
   servo5.attach(drawers[4],500,2500);
   servo6.attach(drawers[5],500,2500);
   
-  //connectToWiFi();
+  connectToWiFi();
 
-    Serial.println("Connecting to WiFi");
-  WiFi.begin("Rafiqul","1311109265");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting...");
-  }
-  Serial.println("Connected to WiFi");
 
   // Initialize Firebase
-  Firebase.begin(&FIREBASE_HOST, &FIREBASE_AUTH);
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+
+  timeClient.begin();
+  timeClient.update();
 }
 
 void loop() {
+  timeClient.update();
+  int currentHour = timeClient.getHours();
+  int currentMinute = timeClient.getMinutes();
+  String currentTime = convertTo12HourFormat(currentHour,currentMinute);
   if(digitalRead(switchPin) == LOW) {
     // Patient pressed the switch indicating medicine taken
 
   }
-
-  time_t currentTime  = now();
-  if(year(currentTime) >= 2023 && month(currentTime) >= 9 && day(currentTime) >= 25) {
-    
-    int currentMinute = hour(currentTime) * 60 + minute(currentTime);
-    /*
-    if(currentMinute == 9*60 + 45) {
-      openDrawer(servo1);
-    } else if(currentMinute == 10*60 + 15) {
-      openDrawer(servo2);
-    } 
-    */
-
     sokale_khawar_age = get_target_time("/user/kawser/Time/sokale_khawar_age");
     sokale_khawar_pore = get_target_time("/user/kawser/Time/sokale_khawar_pore");
     dupure_khawar_age = get_target_time("/user/kawser/Time/dupure_khawar_age");
@@ -87,36 +73,38 @@ void loop() {
     rate_khawar_age = get_target_time("/user/kawser/Time/rate_khawar_age");
     rate_khawar_pore = get_target_time("/user/kawser/Time/rate_khawar_pore");
   
-    if(currentMinute == sokale_khawar_age) {
+    if(currentTime.equals(sokale_khawar_age)) {
       open_Drawer(servo1,55,180);
-    } else if(currentMinute == sokale_khawar_pore) {
+    } else if(currentTime.equals(sokale_khawar_pore) ) {
       open_Drawer(servo2,0,0);
-    }  else if(currentMinute == dupure_khawar_age) {
+    }  else if(currentTime.equals(dupure_khawar_age) ) {
       open_Drawer(servo3,40,180);
-    } else if(currentMinute == dupure_khawar_pore) {
+    } else if(currentTime.equals(dupure_khawar_pore)) {
       open_Drawer(servo4,40,180);
-    } else if(currentMinute == rate_khawar_age) {
-      open_Drawer(servo5,35,180);
-    } else if(currentMinute == rate_khawar_pore) {
+    } else if(currentTime.equals(rate_khawar_age) ) {
+      open_Drawer(servo4,40,180);
+    } else if(currentTime.equals(rate_khawar_pore) ) {
       open_Drawer(servo6,45,180);
     }
-  }
+  
 }
 
-// void connectToWiFi() {
-//   Serial.println("Connecting to WiFi");
-//   WiFi.begin("Rafiqul","1311109265");
-//   while (WiFi.status() != WL_CONNECTED) {
-//     delay(1000);
-//     Serial.println("Connecting...");
-//   }
-//   Serial.println("Connected to WiFi");
 
-//   // Initialize Firebase
-//   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+//--------------------------------------------------------------
+void connectToWiFi() {
+  Serial.println("Connecting to WiFi");
+    WiFi.begin(WIFI_SSID,WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting...");
+  }
+  Serial.println("Connected to WiFi");
 
-// }
+}
 
+
+
+//--------------------------------------------------------------
 void open_Drawer(Servo servo, int Min_Angle, int Max_Angle){
 
     for (int angle = Max_Angle; angle >= Min_Angle; angle -= 1) {
@@ -126,18 +114,22 @@ void open_Drawer(Servo servo, int Min_Angle, int Max_Angle){
 
     while(true) {
       digitalWrite(buzzerPin, HIGH); // Turn on buzzer
-      delay(beepDuration);
-      digitalWrite(buzzerPin, LOW); 
+      delay(100);
+      digitalWrite(buzzerPin, LOW); // Turn off buzzer
     if(digitalRead(switchPin) == LOW) {
       // Patient pressed the switch indicating medicine taken
-
+      close_Drawer(servo,Min_Angle, Max_Angle);
       break; // Exit the loop if switch is pressed
       }
+      delay(drawer_Open_Duration_Time);
+      close_Drawer(servo,Min_Angle, Max_Angle);
     }  
 
-    close_Drawer(servo,Min_Angle, Max_Angle);
 }
 
+
+
+//-----------------------------------------------------------------------
 void close_Drawer(Servo servo, int Min_Angle, int Max_Angle){
 
   for (int angle = Min_Angle; angle <= Max_Angle; angle += 1) {
@@ -146,7 +138,11 @@ void close_Drawer(Servo servo, int Min_Angle, int Max_Angle){
   }
 
 }
-int get_target_time(String path){
+
+
+
+//------------------------------------------------------------------------
+String get_target_time(String path){
   if (Firebase.getString(firebaseData, path)) { // "/user/kawser/Time"
     if (firebaseData.dataType() == "string") {
       String timeString = firebaseData.stringData();
@@ -156,20 +152,25 @@ int get_target_time(String path){
       char amPm[3];
       sscanf(timeString.c_str(), "%d:%d %2s", &hours, &minutes, amPm);
 
-      // Adjust hours for 12-hour format
-      if (strcmp(amPm, "PM") == 0 && hours != 12) {
-        hours += 12;
-      } else if (strcmp(amPm, "AM") == 0 && hours == 12) {
-        hours = 0;
-      }
-
-      // Calculate total minutes since midnight
-      return  (hours * 60) + minutes;
+      String target_time = String(hours) + ":" + (minutes < 10 ? "0" : "") + String(minutes) + " " + amPm;
+      return  target_time;
     }
   } else {
     Serial.println("Error getting value");
     Serial.println(firebaseData.errorReason());
   }
-  return 0;
+}
 
+
+
+//-----------------------------------------------------------------
+String convertTo12HourFormat(int hour, int minute) {
+    String period = (hour >= 12) ? "PM" : "AM";
+
+    if (hour > 12)
+        hour -= 12;
+    else if (hour == 0)
+        hour = 12;
+
+    return String(hour) + ":" + (minute < 10 ? "0" : "") + String(minute) + " " + period;;
 }
